788e0b18e52caa07efdcb8b558afaf705dbe60bb
Noah Goodman <ngoodman@uchicago.edu>
2019-07-15 15:51:31 -0500

    added basic board tiling -- todo, make it infinite

--- src/Main.hs
+++ src/Main.hs
@@ -9,13 +9,15 @@ import Graphics.Gloss.Interface.Environment
 import Graphics.Gloss.Interface.IO.Interact
 import System.Exit
 
+(<>) = mappend
+
 main :: IO ()
 main = do
     screenSize <- getScreenSize
-    interactIO FullScreen white (initWorld screenSize) draw update control
+    interactIO FullScreen blue (initWorld screenSize) draw update control
 
 draw :: World -> IO Picture
-draw (World {..}) = pure . uncurry scale scaling . uncurry translate coords $ board
+draw (World {..}) = pure . uncurry scale scaling . uncurry translate coords $ tiling
 
 update :: Event -> World -> IO World
 --update (EventResize (dx, dy)) (World (x, y)) = pure (World ((x + fromIntegral dx), (y + fromIntegral dy)))
@@ -32,7 +34,7 @@ update (EventKey (MouseButton LeftButton) Up _ location) w@(World{coords = (x,y)
           then pure $ w { coords = (x + 10, y + 10), mouseDown = Nothing }
           else pure $ w { mouseDown = Nothing }
 update (EventKey (MouseButton WheelDown) Down _ _) w@(World { scaling = (x, y)}) = pure $ w { scaling = (x + 0.2, y + 0.2) }
-update (EventKey (MouseButton WheelUp) Down _ _) w@(World { scaling = (x, y)}) = pure $ w { scaling = (x - 0.2, y - 0.2) }
+update (EventKey (MouseButton WheelUp) Down _ _) w@(World { scaling = (x, y)}) = pure $ w { scaling = (max 1 (x - 0.2), max 1 (y - 0.2)) }
 update (EventKey (SpecialKey KeyEsc) Down _ _) _ = exitSuccess
 update (EventMotion (dx, dy)) w@(World {coords = (x,y),..}) =
     case mouseDown of
@@ -53,6 +55,33 @@ row = pictures $ map (\(p,n) -> translate (n*18.3205) 0 p) $ zip (repeat (hexago
 board :: Picture
 board = pictures $ map (\(p,n) -> translate (n * 9.1603) (n * 15.866) p) $ zip (repeat row) [0..10]
 
+rowWithSpace :: Float -> Picture
+rowWithSpace s = pictures $ map (\(p,n) -> translate (n * (2 * 10 * cos (pi / 6) + s)) 0 p) $ zip (repeat (hexagon 10)) [0..10]
+
+boardWithSpace :: Float -> Picture
+boardWithSpace s = pictures $ map (\(p,n) -> translate (n * a * cos (pi / 3)) (n * a * sin (pi / 3)) p) $ zip (repeat (rowWithSpace s)) [0..10]
+  where a = 2 * 10 * cos (pi / 6) + s
+
+borderBoard :: Picture
+borderBoard =  color black (translate (-10 * cos (pi / 6)) (-10 * sin (pi / 6))  $ polygon [(0,0),(10 * 18.3205 + d, 0), (10*18.3205 + 10 * 9.1603 + d + 10 / sqrt 3, 10 * 15.866 + 10), (10 * 9.1603 + 10 / sqrt 3, 10*15.866 + 10)]) <> color white (boardWithSpace 1)
+  where c = 20 * cos (pi / 6)
+        d = 5 / sqrt 3 + 5 * sqrt 3
+
+lattice :: [(Float, Float)]
+lattice = (,) <$> [-5..4] <*> [-3..2]
+
+skew :: (Float, Float) -> (Float, Float)
+skew (x,y) = (c*x + a*y, b*y)
+  where
+    c = 11 * 18.3205
+    b = 11 * 15.866
+    a = 11 * 9.1603
+
+tiling :: Picture
+tiling = 
+    let latticeSkew = map skew lattice
+     in pictures $ map (\(x,y) -> translate x y borderBoard) latticeSkew
+
 -- | draw a regular hexagon with the given lenght from the center to a vertex
 hexagon :: Complex Float -> Picture
 hexagon r = rotate 90 
@@ -64,7 +93,7 @@ hexagon r = rotate 90
 initWorld :: (Int, Int) -> World
 initWorld screen = World 
     { screen = screen
-    , scaling = (1,1)
+    , scaling = (2,2)
     , coords = (0,0) 
     , mouseDown = Nothing
     , dragPos = (0,0)
